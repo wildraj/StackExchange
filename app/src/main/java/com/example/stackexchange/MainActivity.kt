4dacad51e2b7c025ce1fc1f6bab159fe28c0fd27
Rajal Patel <rajalspatel@gmail.com>
2022-09-02 15:25:44 -0500

    Loading Animation

--- app/src/main/java/com/example/stackexchange/MainActivity.kt
+++ app/src/main/java/com/example/stackexchange/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.stackexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.stackexchange.dataclasses.Question
@@ -11,25 +14,65 @@ import androidx.recyclerview.widget.DividerItemDecoration
 
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
+    val questions: MutableList<Question> = arrayListOf()
+    lateinit var questionList: RecyclerView
+    lateinit var layoutManager: LinearLayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Obtains the RecyclerView to display the questions
-        val questionList = findViewById<RecyclerView>(R.id.questionList)
+        // Nested ScrollView
+        val nestedSV = findViewById<NestedScrollView>(R.id.nestedSV)
+        var loadingAnimation = findViewById<ProgressBar>(R.id.loadingAnimation)
 
-        // Some Test Data
-        val questions: MutableList<Question> = arrayListOf()
+        // Obtains the RecyclerView to display the questions
+        questionList = findViewById(R.id.questionList)
+
+        questions.add(Question(2, "Question2", "Comment2"))
+        questions.add(Question(3, "Question3", "Comment3"))
+        questions.add(Question(4, "Question4", "Comment4"))
         questions.add(Question(1, "Question1", "Comment1"))
         questions.add(Question(2, "Question2", "Comment2"))
         questions.add(Question(3, "Question3", "Comment3"))
         questions.add(Question(4, "Question4", "Comment4"))
+        questions.add(Question(1, "Question1", "Comment1"))
+        questions.add(Question(2, "Question2", "Comment2"))
+        questions.add(Question(3, "Question3", "Comment3"))
+        questions.add(Question(4, "Question4", "Comment4"))
+        questionList.adapter = QuestionAdapter(questions)
 
-        // Assigns Linear Layout as the layout manager for the RecyclerView
-        val layoutManager = LinearLayoutManager(this)
+        layoutManager = LinearLayoutManager(this)
         questionList.layoutManager = layoutManager
 
+        // Retrieves the first 10 items
+        getData()
+
+        nestedSV.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+            // on scroll change we are checking when users scroll as bottom.
+            if (scrollY == v.getChildAt(0).measuredHeight - v.measuredHeight) {
+                // in this method we are incrementing page number,
+                // making progress bar visible and calling get data method.
+                count++
+                // on below line we are making our progress bar visible.
+                loadingAnimation.setVisibility(View.VISIBLE)
+                if (count < 20) {
+                    // on below line we are again calling
+                    // a method to load data in our array list.
+                    getData()
+                }
+            }
+        })
+    }
+
+    fun getData(){
+        // Add data
+        questions.add(Question(1, "Question1", "Comment1"))
+
+
         // Binds the obtained data to the RecyclerView
+        // Assigns Linear Layout as the layout manager for the RecyclerView
         questionList.adapter = QuestionAdapter(questions)
         val dividerItemDecoration = DividerItemDecoration(
             questionList.getContext(),
